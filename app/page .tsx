fecba9a41e3873b04c7b7e9cdb4135d8044369e4
aryan1trehan <158035850+aryan1trehan@users.noreply.github.com>
2026-04-25 16:50:07 +0530

    Add files via upload

---
+++ app/page .tsx
@@ -0,0 +1,27 @@
+import Hero from '@/components/Hero'
+import MotionLogo from '@/components/MotionLogos'
+import ManifestoSection from '@/components/ManifestoSection' 
+import BentoFeatures from '@/components/BentoFeatures'
+import ServicesSection from '@/components/ServicesSection'
+import PhilosophySection from '@/components/PhilosophySection'
+import ResultsSection from '@/components/ResultsSection'
+import ContactSection from '@/components/ContactSection'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-bg overflow-x-hidden">
+      <Header />
+      <Hero />
+      <MotionLogo />
+      <ManifestoSection />
+      <BentoFeatures />
+      <ServicesSection />
+      <PhilosophySection />
+      <ResultsSection />
+      <ContactSection />
+      <Footer />
+    </main>
+  )
+}
